ae9357c9911dad941d389cf43e8b2018ae1b8737
Zachary Frangella <60199061+zjf4@users.noreply.github.com>
2024-11-07 12:24:24 -0800

    update pcg.py
    Updates pcg.py with a more efficient version.

--- optimizers/pcg.py
+++ optimizers/pcg.py
@@ -1,5 +1,6 @@
 import jax.numpy as jnp
 from jax import jit
+from jax.lax import while_loop
 from functools import partial
 from typing import NamedTuple
 
@@ -14,39 +15,37 @@ class PCG_State(NamedTuple):
 def pcg(b: jnp.ndarray,
         model: object,
         M: object,
-        max_iter: int):
+        max_iter: int,
+        tol: float = 10**-6):
     
-    return _pcg(b, model, M ,max_iter)
+    return _pcg(b, model, M ,max_iter, tol)
 
 
-@partial(jit,static_argnames=['max_iter'])
+@partial(jit, static_argnames=['max_iter'])
 def _pcg(b: jnp.ndarray,
         model,
         M,
-        max_iter: int):
-    
-    
-    #nits = 0
-
-    # def _cond_fun(state):
-    #   return (jnp.linalg.norm(state.r)>tol) & (state.k<max_iter)
+        max_iter: int,
+        tol: float):
     
     def _init_pcg():
       r = b
-      z = M.batch_apply(r)
+      z = M.apply(r)
       p = jnp.copy(z)
       r_dot_z = jnp.sum(r*z)
       k = 0
-      return PCG_State(jnp.zeros_like(b),r, z, p, r_dot_z, k)
+      return PCG_State(jnp.zeros_like(b),r,z,p,r_dot_z,k)
     
     def _pcg_step(state):
-      w = model.batch_matvec_A(state.p)
+      w = model.matvec_A(state.p)
+      
       # Update solution and residual
       alpha = state.r_dot_z / jnp.sum(w * state.p)
       u = state.u+alpha * state.p
       r = state.r-alpha * w
+      
       # Apply preconditioner
-      z = M.batch_apply(r)
+      z = M.apply(r)
 
       # Update search direction
       rnp1_dot_znp1 = jnp.sum(r * z)
@@ -55,14 +54,13 @@ def _pcg(b: jnp.ndarray,
     
     state = _init_pcg()
 
-    # while _cond_fun(state) == True:
-    #   state = _pcg_step(state)
-    # state =lax.while_loop(_cond_fun,_pcg_step,_init_pcg())
+    def _cond(state):
+        # Stop when residual norm is less than tolerance or max iterations reached
+        return (jnp.linalg.norm(state.r) > tol) & (state.k < max_iter)
   
-    for i in range(max_iter):
-      state = _pcg_step(state)
+    final_state = while_loop(_cond, _pcg_step, state)
 
-    return state.u, state.r, state.k
+    return final_state.u, final_state.r, final_state.k
 
 
 
